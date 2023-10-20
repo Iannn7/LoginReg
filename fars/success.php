4d53e8223ba3cd523f57560e895b368704c74e12
Iannn7 <113899231+Iannn7@users.noreply.github.com>
2023-10-20 13:44:57 +0800

    Add files via upload

---
+++ fars/success.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!-- divinectorweb.com -->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Animated Background using HTML and CSS | No Javascript</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="form.css" />
+  </head>
+  <body>
+    <div class="banner">
+      <div class="content">
+        <form action="info.php" method="post" id="form" >
+          <h1>Registered Successfully</h1>
+          <hr />
+          <p class="succ">You can now <a href="login.php">log in</a></p>
+</body>
+</html>
